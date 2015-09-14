3a5c1b4aac0e050ad7d7d9333983598aed9a9fa0
Matthew Ellsworth <matt.ellsworth@matthewellsworth.com>
2015-09-14 14:27:48 -0500

    gemfile and help command

--- rikc.rb
+++ rikc.rb
@@ -9,9 +9,22 @@ require 'json'
 require 'fileutils'
 require "pty"
 
+# Workflow
+#   - ./rick
+#   - RIKC none > help
+#   - RIKC none > create 1234-customer
+#   - RIKC none > use 1234-customer
+#   - RIKC 1234-customer > list servers
+#   - RIKC 1234-customer > list environments
+#   - RIKC 1234-customer > list databags
+#   - RIKC 1234-customer > list cookbooks
+#   - RIKC 1234-customer > show server servername
+#   - RIKC 1234-customer > show environment environmentname
+#   - RIKC 1234-customer > show databag databagname
+#   - RIKC 1234-customer > show cookbook cookbookname
 
 # Load dotfile to settings
-data = File.read(ENV['HOME']+'/.stabby')
+data = File.read(ENV['HOME']+'/.rikc')
 config = {}
 data.each_line do |line|
   setting = line.split("=")
@@ -25,6 +38,7 @@ Dir["./modules/*.rb"].each {|file| require file }
 buffer = ""
 prompt = ['RIKC ', 'none', ' > ']
 customer_id = ''
+
 # initiate I/O loop
 while true
   # print "RIKC customer >"
@@ -33,19 +47,44 @@ while true
   buffer = STDIN.gets.chomp
   # split input into array
   parts = buffer.split
-  # check for use command
-  if parts[0] == 'use'
+
+  #   - RIKC none > help
+  if parts[0] == 'help'
+    puts %q(
+      Workflow
+        - ./rick
+        - RIKC none > help
+        - RIKC none > create 1234-customer
+        - RIKC none > use 1234-customer
+        - RIKC 1234-customer > list servers
+        - RIKC 1234-customer > list environments
+        - RIKC 1234-customer > list databags
+        - RIKC 1234-customer > list cookbooks
+        - RIKC 1234-customer > show server servername
+        - RIKC 1234-customer > show environment environmentname
+        - RIKC 1234-customer > show databag databagname
+        - RIKC 1234-customer > show cookbook cookbookname
+    )
+
+  #   - RIKC none > create 1234-customer
+  elsif parts[0] == 'create'
+    print "creating customer\n"
+    current_customer = Customer.new(customer_id, config)
+
+  #   - RIKC none > use 1234-customer
+  elsif parts[0] == 'use'
     # set customer ID
     customer_id = parts[1]
     print "customer id set to ", customer_id, "\n"
     # add customer to prompt
     prompt[1] = customer_id
-# check if init
-  elsif parts[0] == "init" and customer_id != ''
-    print "creating customer\n"
-    current_customer = Customer.new(customer_id, config)
-  # enc execution on "exit"
-  elsif buffer == "exit"
+
+  elsif parts[0] == 'list'
+
+  elsif parts[0] == 'show'
+
+  # end execution on "exit"
+  elsif buffer == 'exit'
     exit
   else
     print "unsupported command\n"
